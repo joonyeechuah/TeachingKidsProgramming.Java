7c085ce46e6af0ddb9196a32ce4ab2c47a61e4b5
Ngmangibin <ngmangibin@gmail.com>
2016-01-12 12:34:00 -0600

    Moved and Turned Tortoise

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
@@ -1,6 +1,7 @@
 package org.teachingkidsprogramming.section01forloops;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SimpleSquare
 {
@@ -11,13 +12,16 @@ public class SimpleSquare
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
     //      Change the pen color of the line the tortoise draws to blue --#4
+    Tortoise.setPenColor(PenColors.Blues.Blue);
     //      Move the tortoise 50 pixels --#2
     Tortoise.move(50);
     //      Turn the tortoise to the right (90 degrees) --#3
     Tortoise.turn(90);
     //  Repeat --#5.2
-    //
-    //  (Optional): Sign your work using the Virtual Proctor 
-    //  See your work at http://virtualproctor.tkpjava.org
+    for (int i = 1; i < 4; i++)
+    {
+      //  (Optional): Sign your work using the Virtual Proctor 
+      //  See your work at http://virtualproctor.tkpjava.org
+    }
   }
 }
