818ba987fc9fbcf7e9e643a7e843bb0d731c19ce
Sahil-Patel <sahilpatel12321@gmail.com>
2016-01-14 11:16:03 -0600

    Made a triangular spiral with changing color

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
@@ -1,7 +1,8 @@
 package org.teachingkidsprogramming.section01forloops;
 
 import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Purples;
 import org.teachingextensions.logo.utils.EventUtils.MessageBox;
 
 public class Spiral
@@ -15,13 +16,16 @@ public class Spiral
       //    Make the tortoise go as fast as possible --#4
       Tortoise.setSpeed(10);
       //    Add Blue Violet to the Color Wheel --#7
+      ColorWheel.addColor(Purples.BlueViolet);
       //    Add Violet to the Color Wheel --#8
+      ColorWheel.addColor(Purples.Violet);
       //    Add Purple to the Color Wheel --#9
+      ColorWheel.addColor(Purples.Purple);
       //    Do the following 75 times --#3.1
       try
       {
         //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
-        Tortoise.setPenColor(PenColors.getRandomColor());
+        Tortoise.setPenColor(ColorWheel.getNextColor());
         //         Move the tortoise 5 times the current line number you are drawing --#5
         Tortoise.move(i * 5);
         //         Turn the tortoise 1/3 of 360 degrees to the right --#2
