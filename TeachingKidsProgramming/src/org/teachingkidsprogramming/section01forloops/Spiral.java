d997ad5a95d5636a3ba8da3f7f997de9c53d0f88
Ngmangibin <ngmangibin@gmail.com>
2016-01-14 11:08:46 -0600

    Triangle Spiral

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/Spiral.java
@@ -12,6 +12,7 @@ public class Spiral
     for (int i = 0; i < 75; i++)
     {
       //    Make the tortoise go as fast as possible --#4
+      Tortoise.setSpeed(10);
       //    Add Blue Violet to the Color Wheel --#7
       //    Add Violet to the Color Wheel --#8
       //    Add Purple to the Color Wheel --#9
@@ -20,6 +21,7 @@ public class Spiral
       {
         //         Change the pen color of the line the tortoise draws the next color on the Color Wheel --#6
         //         Move the tortoise 5 times the current line number you are drawing --#5
+        Tortoise.move(5 * i);
         //         Turn the tortoise 1/3 of 360 degrees to the right --#2
         Tortoise.turn(120);
       }
