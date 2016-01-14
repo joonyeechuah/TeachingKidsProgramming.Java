40e5a2637ec7ab0b065304a99e6436a49564b1c9
Ngmangibin <ngmangibin@gmail.com>
2016-01-14 11:04:17 -0600

    Turned Tortoise 75 times

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/SimpleSquare.java
@@ -9,17 +9,17 @@ public class SimpleSquare
   {
     //  Show the tortoise --#1
     Tortoise.show();
-    //  Make the tortoise move as fast as possible --#6
-    //  Do the following 4 times --#5.1
-    //      Change the pen color of the line the tortoise draws to blue --#4
-    Tortoise.setPenColor(PenColors.Blues.Blue);
-    //      Move the tortoise 50 pixels --#2
-    Tortoise.move(50);
-    //      Turn the tortoise to the right (90 degrees) --#3
-    Tortoise.turn(90);
-    //  Repeat --#5.2
-    for (int i = 1; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
+      //  Make the tortoise move as fast as possible --#6
+      //  Do the following 4 times --#5.1
+      //      Change the pen color of the line the tortoise draws to blue --#4
+      Tortoise.setPenColor(PenColors.Blues.Blue);
+      //      Move the tortoise 50 pixels --#2
+      Tortoise.move(50);
+      //      Turn the tortoise to the right (90 degrees) --#3
+      Tortoise.turn(90);
+      //  Repeat --#5.2
       //  (Optional): Sign your work using the Virtual Proctor 
       //  See your work at http://virtualproctor.tkpjava.org
     }
