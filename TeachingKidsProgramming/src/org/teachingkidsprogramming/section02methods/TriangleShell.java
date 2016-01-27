2483031d636e2106459dfdaa6fa3fd960ae7a4f0
Ngmangibin <ngmangibin@gmail.com>
2016-01-27 12:41:30 -0600

    You Suck at Guessing

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/TriangleShell.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section02methods/TriangleShell.java
@@ -1,5 +1,6 @@
 package org.teachingkidsprogramming.section02methods;
 
+import org.teachingextensions.logo.Tortoise;
 
 @SuppressWarnings("unused")
 public class TriangleShell
@@ -8,20 +9,31 @@ public class TriangleShell
   public static void main(String[] args)
   {
     // Show the tortoise --#1
-    // Make the tortoise go as fast as possible --#6
-    // Do the following 60 times --#7.1
-    //      Change the pen color of the line the tortoise draws to a random color --#9
-    //      Increase the current length of the side by 4 pixels --#8
-    //      drawTriangle (recipe below) --#5.1
-    //
-    //      ------------- Recipe for drawTriangle --#5.2
+    Tortoise.show();
+    for (int i = 0; i < 3; i++)
+    {
+      // Make the tortoise go as fast as possible --#6
+      Tortoise.setSpeed(10);
+      // Do the following 60 times --#7.1
+      //      Change the pen color of the line the tortoise draws to a random color --#9
+      //      Increase the current length of the side by 4 pixels --#8
+      //      drawTriangle (recipe below) --#5.1
+      //
+      //      ------------- Recipe for drawTriangle --#5.2
+      drawTriangle();
+      //      ------------- End of drawTriangle recipe --#5.3
+      //
+      //      Turn the tortoise 1/60th of 360 degrees to the right --#10
+      // Repeat --#7.2
+    }
+  }
+  private static void drawTriangle()
+  {
     //          Do the following 3 times --#3.1
     //              Move the tortoise using the current length --#4
+    Tortoise.move(1);
     //              Turn the tortoise 1/3rd of 360 degrees --#2
+    Tortoise.turn(120);
     //          Repeat --#3.2
-    //      ------------- End of drawTriangle recipe --#5.3
-    //
-    //      Turn the tortoise 1/60th of 360 degrees to the right --#10
-    // Repeat --#7.2
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLow.java
@@ -1,21 +1,44 @@
 package org.teachingkidsprogramming.section03ifs;
 
+import org.teachingextensions.logo.Sound;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
 public class HiLow
 {
   public static void main(String[] args)
   {
-    //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
-    //    Do the following 8 times --#9.1
-    //         Ask the user for a guess --#1
-    //         If the guess is correct --#4
-    //               Play a bell --#2
-    //               Tell the user that they won the game  --#3
-    //               and exit --#10
-    //         Otherwise, if the guess is too high --#6
-    //               Tell the end user that it is too high --#5
-    //         Otherwise, if the guess is too low --#8
-    //               Tell the end user that it is too low --#7
-    //    Repeat --#9.2
+    for (int i = 0; i < 8; i++)
+    {
+      //    Choose a random number between 1 and 100 --#4.1 (fake!) & --#13
+      //    Do the following 8 times --#9.1
+      //         Ask the user for a guess --#1
+      int guess = MessageBox.askForNumericalInput("guess");
+      if (guess == 57)
+      {
+        //         If the guess is correct --#4
+        //               Play a bell --#2
+        Sound.playBeep();
+        //               Tell the user that they won the game  --#3
+        MessageBox.showMessage("You Won!!");
+        //               and exit --#10
+        System.exit(0);
+      }
+      //         Otherwise, if the guess is too high --#6
+      else if (guess >= 57)
+      {
+        //               Tell the end user that it is too high --#5
+        MessageBox.showMessage("2Hi");
+      }
+      //         Otherwise, if the guess is too low --#8
+      else if (guess <= 57)
+      {
+        //               Tell the end user that it is too low --#7
+        MessageBox.showMessage("2Low");
+      }
+      //    Repeat --#9.2
+    }
     //    After 8 incorrect guesses tell the user they've lost --#11
+    MessageBox.showMessage(
+        "IF I WANTED TO KILL MYSELF I'D JUMP FROM YOUR EGO DOWN TO YOUR IQ. JUST GO DIE IN A HOLE CAUSE YOU SUCK AT GUESSING. IDIOT!!!");
   }
 }
