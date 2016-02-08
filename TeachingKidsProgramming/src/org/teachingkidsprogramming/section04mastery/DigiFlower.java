8a2dea42751d0ca01f77afd620eb6f72f2644834
Sahil-Patel <sahilpatel12321@gmail.com>
2016-02-08 13:00:49 -0600

    Finished Digiflower

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/DigiFlower.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/DigiFlower.java
@@ -2,22 +2,56 @@ package org.teachingkidsprogramming.section04mastery;
 
 import java.awt.Color;
 
+import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Grays;
 
 public class DigiFlower
 {
   public static void main(String[] args)
   {
     //    Show the tortoise --#1
+    Tortoise.show();
     //    Make the tortoise move as fast as possible --#7
+    Tortoise.setSpeed(10);
     //    Make the background silver  --#8
+    Tortoise.getBackgroundWindow().setBackground(Grays.Silver);
     //    Make the line the tortoise draws 3 pixels wide --#15
-    //    createColorPalette (recipe below) --#9.1
-    //        Do the following 15 times --#13.1
-    //          drawOctogon (recipe below) --#10.1
-    //          Turn the tortoise 1/15th of 360 degrees to the right --#12
-    //        Repeat --#14.2
+    Tortoise.setPenWidth(3);
+    //    createColorPalette (recipe below) --#9.1  
+    {
+      createColorPalette();
+      //        Do the following 15 times --#13.1
+      for (int i = 0; i < 15; i++)
+      {
+        //          drawOctogon (recipe below) --#10.1
+        drawOctagon();
+        //          Turn the tortoise 1/15th of 360 degrees to the right --#12
+        Tortoise.turn(360 / 15);
+      }
+      //        Repeat --#14.2
+      //
+    }
+  }
+  private static void drawOctagon()
+  {
+    //    ------------- Recipe for drawOctogon --#10.2
+    for (int i = 0; i < 8; i++)
+    {
+      //    Do the following 8 times --#6.1
+      //      Change the pen color of the line the tortoise draws to the next color on the color wheel --#4
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      //      Move the tortoise 50 pixels --#2
+      Tortoise.move(50);
+      //      Turn the tortoise 1/8th of 360 degrees to the right --#5
+      Tortoise.turn(360 / 8);
+    }
+    //    Repeat --#6.2 
+    //    ------------- End of drawOctogon recipe --#10.3
+  }
+  private static void createColorPalette()
+  {
     //    ------------- Recipe for createColorPalette --#9.2
     Color color1 = PenColors.Reds.Red;
     Color color2 = PenColors.Oranges.DarkOrange;
@@ -32,13 +66,5 @@ public class DigiFlower
     ColorWheel.addColor(color2);
     ColorWheel.addColor(color1);
     //    ------------- End of createColorPalette recipe --#9.3
-    //
-    //    ------------- Recipe for drawOctogon --#10.2
-    //    Do the following 8 times --#6.1
-    //      Change the pen color of the line the tortoise draws to the next color on the color wheel --#4
-    //      Move the tortoise 50 pixels --#2
-    //      Turn the tortoise 1/8th of 360 degrees to the right --#5
-    //    Repeat --#6.2 
-    //    ------------- End of drawOctogon recipe --#10.3
   }
 }
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -1,6 +1,9 @@
 package org.teachingkidsprogramming.section04mastery;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Blues;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors.Purples;
 
 public class PentagonCrazy
 {
@@ -8,34 +11,65 @@ public class PentagonCrazy
   {
     Tortoise.show();
     //    Make the tortoise move as fast as possible --#3
+    Tortoise.setSpeed(10);
+    createColorPalette();
+    //
+    //    drawPentagon (recipe below) --#10.1
+    //
+    drawPentagon();
+  }
+  private static void drawPentagon()
+  {
+    //    ------------- Recipe for drawPentagon --#10.2
+    //    Do the following 200 times --#2.1
+    for (int i = 0; i < 200; i++)
+    {
+      //        adjustPen (recipe below) --#9.1
+      //
+      adjustPen();
+      //
+      //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
+      int lengthOfSide = i;
+      //        Move the tortoise the length of a side --#4.1
+      Tortoise.move(lengthOfSide);
+      //        Turn the tortoise 1/5th of 360 degrees --#1
+      Tortoise.turn(360 / 5);
+      //        Turn the tortoise 1 more degree --#5
+      Tortoise.turn(1);
+      //    Repeat --#2.2
+    }
+    //    ------------- End of drawPentagon recipe --#10.3
+  }
+  private static void adjustPen()
+  {
+    //        ------------- Recipe for adjustPen --#9.2
+    //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
+    Tortoise.setPenColor(ColorWheel.getNextColor());
+    //        Increase the tortoises pen width by 1 --#15
+    Tortoise.setPenWidth(Tortoise.getPenWidth() + 1);
+    //        If the tortoises pen width is greater than 4, then --#17
+    if (Tortoise.getPenWidth() > 4)
+    {
+      //            Reset it to 1 --#16
+      Tortoise.setPenWidth(1);
+    }
+    //        ------------- End of adjustPen recipe --#9.3 
+  }
+  private static void createColorPalette()
+  {
     //    createColorPalette (recipe below) --#8.1
     //
     //    ------------- Recipe for createColorPalette --#8.2
     //    Add steel blue to the color wheel --#7
+    ColorWheel.addColor(Blues.SteelBlue);
     //    Add dark orchid to the color wheel --#11
+    ColorWheel.addColor(Purples.DarkOrchid);
     //    Add dark slate blue to the color wheel --#12
+    ColorWheel.addColor(Blues.DarkSlateBlue);
     //    Add teal to the color wheel --#13
+    ColorWheel.addColor(Blues.Teal);
     //    Add indigo to the color wheel --#14
+    ColorWheel.addColor(Purples.Indigo);
     //    ------------- End of createColorPalette recipe --#8.3
-    //
-    //    drawPentagon (recipe below) --#10.1
-    //
-    //    ------------- Recipe for drawPentagon --#10.2
-    //    Do the following 200 times --#2.1
-    //        adjustPen (recipe below) --#9.1
-    //
-    //        ------------- Recipe for adjustPen --#9.2
-    //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
-    //        Increase the tortoises pen width by 1 --#15                                              
-    //        If the tortoises pen width is greater than 4, then --#17
-    //            Reset it to 1 --#16
-    //        ------------- End of adjustPen recipe --#9.3 
-    //
-    //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
-    //        Move the tortoise the length of a side --#4.1
-    //        Turn the tortoise 1/5th of 360 degrees --#1
-    //        Turn the tortoise 1 more degree --#5
-    //    Repeat --#2.2
-    //    ------------- End of drawPentagon recipe --#10.3
   }
 }
